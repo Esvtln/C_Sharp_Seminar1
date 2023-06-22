e5b29a2b2999d456b2f037b3914e17f1731bff84
Svetlana <azogam@gmail.com>
2023-06-22 14:26:35 +0300

    Solution2

--- Program.cs
+++ Program.cs
@@ -3,19 +3,56 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
-Console.WriteLine("Введите число №1: ");
+// Console.WriteLine("Введите число №1: ");
+// string number1 = Console.ReadLine()!;
+// int num1 = int.Parse(number1);
+
+// Console.WriteLine("Веедите число №2: ");
+// string number2 = Console.ReadLine()!;
+// int num2 = int.Parse(number2);
+
+// if (num1 > num2)
+// {
+//     Console.WriteLine($"Максимальное число: {num1}");
+// }
+// else 
+// {
+//     Console.WriteLine($"Максимальное число: {num2}");
+// }
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите первое число ");
 string number1 = Console.ReadLine()!;
 int num1 = int.Parse(number1);
 
-Console.WriteLine("Веедите число №2: ");
+Console.WriteLine("Введите второе число ");
 string number2 = Console.ReadLine()!;
 int num2 = int.Parse(number2);
 
-if (num1 > num2)
+Console.WriteLine("Введите третье число ");
+string number3 = Console.ReadLine()!;
+int num3 = int.Parse(number3);
+
+if (num1> num2)
+    if (num1 > num3)
+    {
+        Console.WriteLine($"Max = {num1} ");
+    }
+    else
+    {
+        Console.WriteLine($"Max = {num3} ");
+    }
+else if (num2 > num3)
 {
-    Console.WriteLine($"Максимальное число: {num1}");
+    Console.WriteLine($"Max = {num2} ");
 }
 else 
 {
-    Console.WriteLine($"Максимальное число: {num2}");
+    Console.WriteLine($"Max = {num3} ");
 }
+   
+
